a1b496ced8468fd4438adea993b4a7a271c5fc34
BraydenPowell <86811660+BraydenPowell@users.noreply.github.com>
2022-07-21 15:19:25 +1000

    Update StrengthInNumbers.py

--- StrengthInNumbers.py
+++ StrengthInNumbers.py
@@ -21,25 +21,29 @@ def getMyPosition(prcSoFar):
     negrange = [0, 20, 24, 25, 27, 30, 42, 45, 46, 50, 56, 64, 77, 84, 87, 88, 90]
     global count
     for i in range(0,100):
-        ev[i] = 0
         if i not in negrange:
             if std_of_price_grad[i]>0.15 and prcSoFar.iloc[nt-1, i] > short_ema.iloc[nt-1, i] and  grad_short_ema.iloc[nt-1, i] < 0 :
-                rpos[i] += sell
+                if rpos[i] >= -10000:
+                    rpos[i] += sell
                 
             if std_of_price_grad[i]>0.15 and prcSoFar.iloc[nt-1, i] < short_ema.iloc[nt-1, i] and grad_short_ema.iloc[nt-1, i] > 0:
-                rpos[i] += buy  
+                if rpos[i] <= 10000:
+                    rpos[i] += buy  
                 
             if std_of_price[i]<0.01 and grad_short_ema.iloc[nt-1, i]  <0:
-                rpos[i] += sell 
+                if rpos[i] >= -10000:
+                    rpos[i] += sell 
                 
             if std_of_price[i]<0.01 and grad_short_ema.iloc[nt-1, i] > 0:
-                rpos[i] += buy 
+                if rpos[i] <= 10000:    
+                    rpos[i] += buy 
                 
             if std_of_price_grad[i]<0.01 and grad_long_ema.iloc[nt-1, i]  <0:
-                rpos[i] += sell
+                if rpos[i] >= -10000:
+                    rpos[i] += sell
                 
             if std_of_price_grad[i]<0.01 and grad_long_ema.iloc[nt-1, i] > 0:
-                rpos[i] += sell
-
+                if rpos[i] >= -10000:    
+                    rpos[i] += sell
     currentPos = rpos
     return currentPos
